ae383d551bac04c44ed2abf41546088ca86721fd
Rajat Shet <rajat.shet@jungleegames.com>
2023-01-05 10:32:02 +0530

    ui change

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,10 +7,39 @@ export function Home() {
         <div className={styles.igLogoDiv}>
             <img className={styles.igLogo} src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/2048px-Instagram_logo_2016.svg.png"/>
             <div className={styles.gap}></div>
-            <h3 className={styles.awb}>/ @aWeirdBloke</h3>
+            <p className={styles.awb}>/ @aWeirdBloke</p>
+        </div>
+        <div className={styles.sub}>
+            <div>
+                <p>
+                    Moments
+                </p>
+            </div>
+            <h3>
+                |
+            </h3>
+            <div>
+                <p>
+                   Videos
+                </p>
+            </div>
+            <h3>
+                |
+            </h3>
+            <div>
+                <p>
+                    Edits
+                </p>
+            </div>
+            <h3>
+                |
+            </h3>
+            <div>
+                <p>
+                    Life
+                </p>
+            </div>
         </div>
         <p className={styles.note}>Site under construction...</p>
-
-
     </div>
 }
